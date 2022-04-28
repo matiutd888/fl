55b8add2ab21b69b66c83841b70a600aba0c11de
Mateusz <matiutd888@gmail.com>
2022-04-28 19:38:28 +0200

    Unify typechecking of binary operators

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -1,8 +1,11 @@
+-# LANGUAGE FlexibleContexts #-}
+
 import AbsGramatyka as A
 import Control.Monad.Except
 import Control.Monad.Identity
 import Control.Monad.Reader
 import Control.Monad.State
+
 import qualified Data.Map as M
 import Data.Text
 import PrintGramatyka
@@ -18,23 +21,38 @@ typeOfExpr :: A.Expr -> ExprTEval A.Type
 typeOfExpr (A.ELitTrue pos) = return $ A.Bool pos
 typeOfExpr (A.ELitFalse pos) = return $ A.Bool pos
 typeOfExpr (A.ELitInt pos _) = return $ A.Int pos
--- Boolean operator expression expressions.
-typeOfExpr (A.EAnd pos e1 e2) = typeOfBoolOpExp pos e1 e2
-typeOfExpr (A.EOr pos e1 e2) = typeOfBoolOpExp pos e1 e2
+typeOfExpr (A.EString pos _) = return $ A.Str pos
+-- Binary operator expressions.
+typeOfExpr (A.EAnd pos e1 e2) = typeOfBinOp checkForBool A.Bool pos e1 e2
+typeOfExpr (A.EOr pos e1 e2) = typeOfBinOp checkForBool A.Bool pos e1 e2
+typeOfExpr (A.EAdd pos e1 _ e2) = typeOfBinOp checkForInt A.Int pos e1 e2
+typeOfExpr (A.EMul pos e1 _ e2) = typeOfBinOp checkForInt A.Int pos e1 e2
 
 -- TODO sprawdzić czy to działa
-typeOfBoolOpExp pos e1 e2 = do
-  t1 <- typeOfExpr e1 >>= checkForBool
-  t2 <- typeOfExpr e2 >>= checkForBool
-  return $ A.Bool pos
+-- TODO ujednolicić te cztery funkcje
+typeOfBinOp ::
+     (Type -> ExprTEval ())
+  -> (BNFC'Position -> A.Type)
+  -> A.BNFC'Position
+  -> A.Expr
+  -> A.Expr
+  -> ExprTEval A.Type
+typeOfBinOp checkFunction typeConstructor pos e1 e2 = do
+  typeOfExpr e1 >>= checkFunction
+  typeOfExpr e2 >>= checkFunction
+  return $ typeConstructor pos
 
-typeOfExpr _ = undefined
-
-checkForBool :: Type -> ExprTEval ()
+checkForBool :: MonadError String m => Type -> m ()
 checkForBool t =
   case t of
     A.Bool _ -> return ()
     t -> throwError (errorMessage1 t $ A.Bool $ A.hasPosition t)
+
+checkForInt :: MonadError String m => Type -> m ()
+checkForInt t =
+  case t of
+    A.Int _ -> return ()
+    t -> throwError (errorMessage1 t $ A.Int $ A.hasPosition t)
 
 errorMessage1 :: Type -> Type -> String
 errorMessage1 received expected =
