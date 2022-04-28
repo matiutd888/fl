883691464aae57e0ea6b499b9b45d29393ae9674
Mateusz <matiutd888@gmail.com>
2022-04-28 03:34:50 +0200

    Rename and move script

---
+++ copy-essential-files.sh
@@ -0,0 +1,3 @@
+cp build/AbsGramatyka.hs src/
+cp build/PrintGramatyka.hs src/
+cp build/LexGramatyka.hs src/
--- src/copy_essential_files.sh
+++
@@ -1,3 +0,0 @@
-cp ../build/AbsGramatyka.hs .
-cp ../build/PrintGramatyka.hs .
-cp ../build/LexGramatyka.hs .
