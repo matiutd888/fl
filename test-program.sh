3b11dd94beb12ce9674fdd79addda9a6c842f637
Mateusz <matiutd888@gmail.com>
2022-05-11 03:05:41 +0200

    Refactor test-program.sh

--- test-program.sh
+++ test-program.sh
@@ -1,5 +1,14 @@
 cd build/
+if [ ! -f ../przyklady/$1 ]; then
+    echo "File przyklady/$1 does not exist!"
+    exit 1
+fi
 ./TestGramatyka <../przyklady/$1 >../src/TestProgram.hs
+if [ ! $? -eq 0 ]; then
+    echo "Syntax error with the file"
+    cat ../src/TestProgram.hs
+    exit 1
+fi
 cd ../src/
 ghc -o main Main.hs
 rm -rf *.o *.hi
