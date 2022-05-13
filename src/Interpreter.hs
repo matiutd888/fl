dbc9151fe36283c22dc920f4cb08423c623f44c1
Mateusz <matiutd888@gmail.com>
2022-05-13 21:19:59 +0200

    Implement statement interpreter

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -42,6 +42,28 @@ data FunctionData =
     }
   deriving (Prelude.Eq, Prelude.Ord, Prelude.Show)
 
+propagateFlags :: Env -> EvalT Env
+propagateFlags newEnv = ask >>= \env -> return $ env {hasReturn = hasReturn newEnv}
+
+newLoc :: MonadState Store m => m Loc
+newLoc = do
+  s <- get
+  let l = newloc s
+  put $ s {newloc = l + 1}
+  return l
+
+putData :: Loc -> Data -> EvalT ()
+putData l d = do
+  s <- get
+  put $ s {memory = M.insert l d (memory s)}
+  return ()
+
+safeLookup :: Ord b => String -> b -> M.Map b a -> EvalT a
+safeLookup errorMsg b m = do
+  case M.lookup b m of
+    Nothing -> throwError $ "SAFELOOKUP ERROR " ++ errorMsg
+    Just s -> return s
+
 data Env =
   Env
     { variables :: M.Map A.Ident Loc
@@ -60,38 +82,25 @@ data Data
   | Bool Bool
   | Tuple [Data]
   | Function FunctionData
+  | Void
+  | NODATA
   deriving (Prelude.Eq, Prelude.Ord, Prelude.Show)
 
 -- Maybe we will need to store also something else in the future.
 data Store =
   Store
     { memory :: M.Map Loc Data
+    , newloc :: Loc
     }
 
 type EvalT a = ReaderT Env (StateT Store (ExceptT String Identity)) a
-    -- ~ = ETuple a [Expr' a] DONE
-    -- ~ | EVar a Ident DONE
-    -- ~ | ELitInt a Integer DONE
-    -- ~ | ELitTrue a DONE
-    -- ~ | ELitFalse a DONE
-    -- ~ | ELambda a (Lambda' a) DONE
-    -- ~ | EApp a (Callee' a) [Expr' a]
-    -- ~ | EString a String DONE
-    -- ~ | Neg a (Expr' a) DONE
-    -- ~ | Not a (Expr' a) DONE
-    -- ~ | EMul a (Expr' a) (MulOp' a) (Expr' a) DONE
-    -- ~ | EAdd a (Expr' a) (AddOp' a) (Expr' a) DONE
-    -- ~ | ERel a (Expr' a) (RelOp' a) (Expr' a) DONE
-    -- ~ | EAnd a (Expr' a) (Expr' a) DONE
-    -- ~ | EOr a (Expr' a) (Expr' a) DONE
-  -- ~ deriving (C.Eq, C.Ord, C.Show, C.Read, C.Functor, C.Foldable, C.Traversable)
 
 typeCheckerError :: String
 typeCheckerError = "TYPECHECKER ERROR "
 
--- ~ type Expr = Expr' BNFC'Position
--- ~ data Expr' a
--- ~ fromInt :: MonadError String m => A.BNFC'Position -> Data -> m Int
+runTimeError :: String
+runTimeError = "Runtime error: "
+
 fromInt :: A.BNFC'Position -> Data -> EvalT Integer
 fromInt pos (Int i) = return i
 fromInt pos d =
@@ -174,7 +183,10 @@ evalExpr (A.EVar pos ident) = do
         Nothing ->
           throwError $ typeCheckerError ++ (undefinedReferenceMessage ident pos)
         Just f -> return $ Function f
-    Just l -> return $ DM.fromJust (M.lookup l $ memory s)
+    Just l -> case DM.fromJust (M.lookup l $ memory s) of
+                NODATA -> throwError $ runTimeError ++ showPosition pos ++ " attempting to get the value of uinitialized value " ++ printTree ident
+                x -> return x
+
 evalExpr (A.ETuple pos expressions) = do
   values <- mapM evalExpr expressions
   return $ Tuple values
@@ -206,9 +218,141 @@ evalWithLoc e@(A.EVar pos ident) = do
 evalWithLoc e =
   throwError $ typeCheckerError ++ showPositionOf e ++ "not a variable"
 
-handleFunction :: FunctionData -> [A.Expr] -> handle
-handleFunction = undefined
+-- Wywołuje funkcje function
+handleFunction :: FunctionData -> [A.Expr] -> EvalT Data
+handleFunction f@(FunctionData env stmt arguments retType) exprs = undefined
 
+-- I'll do it later.
+-- Tutaj będę iterował się po argumentach
+-- Jezeli będzie typ referencyjny to wezmę lokację expression i zapiszę pod daną zmienną
+-- jeżeli będzie normalny to zewaluuję wyrażenie, zalokuję nową zmienną i do stora
+-- w storze trzymając wartość tego wyrażenia.
+-- ~ -- Ewaluacja wyrażenia jest w innym środowisku niż 
+handleArg :: A.Arg -> A.Expr -> Env -> EvalT Env
+handleArg (A.Arg _ (A.ArgT pos t) ident) expr envToAdd = do
+  d <- evalExpr expr
+  l <- newLoc
+  putData l d
+  return envToAdd {variables = M.insert ident l (variables envToAdd)}
+handleArg (A.Arg _ (A.ArgRef pos t) ident) expr envToAdd = do
+  l <- evalWithLoc expr
+  return envToAdd {variables = M.insert ident l (variables envToAdd)}
+
+-- Typing statements
+evalStmt :: A.Stmt -> EvalT Env
+evalStmt (A.Empty pos) = ask >>= return
+evalStmt (A.Ass pos ident expr) = do
+  env <- ask
+  d <- evalExpr expr
+  l <- safeLookup (showPosition pos ++ typeCheckerError) ident (variables env)
+  putData l d
+  ask >>= return
+evalStmt (A.Cond pos expr stmt) = do
+  b <- evalExpr expr >>= fromBool pos
+  if b
+    then evalStmt stmt >>= propagateFlags >>= return
+    else ask >>= return
+evalStmt (A.CondElse pos e s1 s2) = do
+  b <- evalExpr e >>= fromBool pos
+  (if b
+     then evalStmt s1
+     else evalStmt s2) >>= propagateFlags >>= return
+evalStmt s@(A.While pos e stmt) = do
+  b <- evalExpr e >>= fromBool pos
+  if b
+    then do
+      e <- evalStmt stmt >>= propagateFlags 
+      if (hasReturn e) then return e else evalStmt s
+    else ask >>= return
+evalStmt (A.SExp pos e) = do
+  evalExpr e
+  ask >>= return
+evalStmt (A.Ret pos e) = do
+  d <- evalExpr e
+  putData retLoc d
+  e <- ask
+  return $ e {hasReturn = True}
+evalStmt (A.VRet pos) = do
+  putData retLoc Void
+  e <- ask
+  return $ e {hasReturn = True}
+evalStmt (A.BStmt _ (A.Block _ stmts)) = do
+  env <- ask
+  foldM handleStmtWithNewEnv env stmts >>= propagateFlags >>= return
+  where
+    handleStmtWithNewEnv :: Env -> A.Stmt -> EvalT Env
+    handleStmtWithNewEnv e s = do
+      if (hasReturn e)
+        then return e
+        else local (\_ -> e) (evalStmt s)
+evalStmt (A.DeclStmt pos (A.Decl _ t items)) = do
+  env <- ask 
+  foldM handleItem env items
+  where    
+    handleItem :: Env -> A.Item -> EvalT Env
+    handleItem newEnv (A.Init pos ident e) = do
+      d <- local (\_ -> newEnv) $ evalExpr e
+      l <- newLoc
+      putData l d
+      return newEnv { variables = M.insert ident l (variables newEnv) }
+
+    handleItem newEnv (A.NoInit pos ident) = do
+      l <- newLoc
+      putData l NODATA
+      return newEnv { variables = M.insert ident l (variables newEnv) }
+evalStmt (A.DeclStmt _ (A.FDecl pos retType ident args b)) = do
+  en <- ask
+  return $ en {functions = M.insert ident (fix (phi en)) (functions en)}
+  -- Alternative, without fixpoint.
+  -- let newEnv =
+  --       en
+  --         { functions =
+  --             M.insert
+  --               ident
+  --               (FunctionData newEnv (A.BStmt (A.hasPosition b) b) args retType)
+  --               (functions env)
+  --         }
+  where
+    phi :: Env -> FunctionData -> FunctionData
+    phi e f =
+      FunctionData
+        (e {functions = M.insert ident f (functions e)})
+        (A.BStmt (A.hasPosition b) b)
+        args
+        retType
+evalStmt (TupleAss a [TupleIdent' a] (Expr' a))
+
+
+-- type Item = Item' BNFC'Position
+-- data Item' a = NoInit a Ident | Init a Ident (Expr' a)
+--   deriving (C.Eq, C.Ord, C.Show, C.Read, C.Functor, C.Foldable, C.Traversable)
+
+-- data FunctionData =
+--   FunctionData
+--     { env :: Env
+--     , stmt :: A.Stmt
+--     , arguments :: [A.Arg]
+--     , retType :: A.Type
+--     }
+-- type Decl = Decl' BNFC'Position
+-- data Decl' a
+--     = Decl a (Type' a) [Item' a]
+--     | FDecl a (Type' a) Ident [Arg' a] (Block' a)
+-- evalStmt
+-- type Stmt = Stmt' BNFC'Position
+-- data Stmt' a
+--     = Empty a DONE
+--     | BStmt a (Block' a) DONE
+--     | DeclStmt a (Decl' a)  DONE
+--     | TupleAss a [TupleIdent' a] (Expr' a)
+--     | Ass a Ident (Expr' a) DONE
+--     | Ret a (Expr' a) DONE
+--     | VRet a DONE
+--     | Cond a (Expr' a) (Stmt' a) DONE
+--     | CondElse a (Expr' a) (Stmt' a) (Stmt' a) DONE
+--     | While a (Expr' a) (Stmt' a) DONE
+--     | SExp a (Expr' a) DONE
+--   deriving (C.Eq, C.Ord, C.Show, C.Read, C.Functor, C.Foldable, C.Traversable)
 -- ~ type Lambda = Lambda' BNFC'Position
 -- ~ data Lambda' a = Lambda a [Arg' a] (Type' a) (Block' a)
 operation :: A.RelOp -> (Integer -> Integer -> Bool)
