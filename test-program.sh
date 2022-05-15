48528e2078f3df8dc5e839d5598e310d08ee00ca
Mateusz <matiutd888@gmail.com>
2022-05-15 04:09:26 +0200

    Test the program

--- test-program.sh
+++ test-program.sh
@@ -1,15 +1,14 @@
-cd build/
-if [ ! -f ../przyklady/$1 ]; then
-    echo "File przyklady/$1 does not exist!"
+if [ ! -f $1 ]; then
+    echo "File $1 does not exist!"
     exit 1
 fi
-./TestGramatyka <../przyklady/$1 >../src/TestProgram.hs
+build/./TestGramatyka <$1 >src/TestProgram.hs
 if [ ! $? -eq 0 ]; then
     echo "Syntax error with the file"
     cat ../src/TestProgram.hs
     exit 1
 fi
-cd ../src/
+cd src/
 ghc -o main Main.hs
 rm -rf *.o *.hi
 ./main
